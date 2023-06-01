58b21c1336061934ce6c081d27a8e2327479bad6
AlexSimenko <simenkoalex2@gmail.com>
2023-06-02 00:01:31 +0300

    New commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("Hi!");
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("I love Nastya");
+        }
     }
+
 }
